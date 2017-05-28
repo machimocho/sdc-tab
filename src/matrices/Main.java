0cb4d35114088f24017c91245fc0753c030febd8
Mario Alvarez <machimocho@gmail.com>
2017-05-28 12:35:11 -0500

    Clase principal

--- src/matrices/Main.java
+++ src/matrices/Main.java
@@ -1,5 +1,5 @@
 package matrices;
 
 public class Main {
-
+	
 }
---
+++ src/matrices/Matriz.java
@@ -0,0 +1,15 @@
+package matrices;
+
+import java.util.ArrayList;
+
+public class Matriz {
+	private ArrayList <ArrayList <Integer>> matriz;
+
+	public ArrayList<ArrayList<Integer>> getMatriz() {
+		return matriz;
+	}
+
+	public void setMatriz(ArrayList<ArrayList<Integer>> matriz) {
+		this.matriz = matriz;
+	} 
+}
