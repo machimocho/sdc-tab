37b80ecf6a025f7857dcdb795d073682611b167b
neburnz <neburnz@hotmail.com>
2017-05-28 22:05:04 +0400

    Se agregan archivos.

---
+++ src/matrices/CalculoMatriz.java
@@ -0,0 +1,12 @@
+package matrices;
+
+public class CalculoMatriz {
+
+	public Matriz Multiplicar(Matriz a, Matriz b) {
+		return new Matriz();
+	}
+	
+	public boolean EsValidoMultiplicar(Matriz a, Matriz b) {
+		return false;
+	}
+}
